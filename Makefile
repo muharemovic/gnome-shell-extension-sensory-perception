4b42a5cf19775e6a16c2933378b7f5874469fc53
Kevin McCormack <harlemsquirrel@gmail.com>
2018-09-18 08:16:09 -0400

    Update package.json and remove Makefile

--- Makefile
+++
@@ -1,9 +0,0 @@
-VERSION = $(shell underscore extract --in metadata.json 'version')
-UUID = $(shell underscore extract --in metadata.json --outfmt text 'uuid')
-FILENAME="$(UUID)-v$(VERSION).zip"
-
-zip:
-	zip -rT $(FILENAME) icons po schemas *.js *.json *.css LICENSE README.md
-
-watch_logs:
-	journalctl /usr/bin/gnome-shell -f -o cat
---
+++ build-zip.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Build the zip file with the current version read from metadata.json
+# using underscore
+
+command -v underscore >/dev/null 2>&1 || {
+  echo >&2 "Please install underscore with \"npm install\".  Aborting."
+  exit 1
+}
+
+VERSION="$(underscore extract --in metadata.json --outfmt text 'version')"
+UUID="$(underscore extract --in metadata.json --outfmt text 'uuid')"
+FILENAME="$UUID-v$VERSION.zip"
+
+printf "Zipping into $FILENAME...\n"
+zip -r $FILENAME icons po schemas *.js *.json LICENSE README.md
+printf "done!\n"
