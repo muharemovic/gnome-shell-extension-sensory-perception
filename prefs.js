9383a15e919c8de8c3db23294ae768434c7223f3
Kevin McCormack <harlemsquirrel@mccormack.tech>
2021-06-21 01:01:54 -0400

    Update prefs

--- prefs.js
+++ prefs.js
@@ -181,11 +181,11 @@ class SensorsPrefsWidget extends Gtk.Grid {
         this.attach(checkButton, 4, topOffset, 2, 1);
 
         this.attach(new Gtk.Label({ label: 'Sensory Perception version: ' + Me.metadata['version'], xalign: 0 }), 2, ++topOffset, 4, 1);
-        this.attach(new Gtk.Label({ label: this._getSensorsVersion(), wrap: true, xalign: 0 }), 2, ++topOffset, 4, 1);
 
         const sensorsCmd = Utilities.detectSensors().join(' ');
-        this.attach(new Gtk.Label({ label: _("Sensor command path"), xalign: 1 }), 0, ++topOffset, 2, 1);
+        this.attach(new Gtk.Label({ label: _("lm-sensors"), xalign: 1 }), 0, ++topOffset, 2, 1);
         this.attach(new Gtk.Label({ label: sensorsCmd, xalign: 1 }), 1, topOffset, 2, 1);
+        this.attach(new Gtk.Label({ label: this._getSensorsVersion(), wrap: true, xalign: 0 }), 2, ++topOffset, 4, 1);
     }
 
     _comboBoxSeparator(model, iter, data) {
